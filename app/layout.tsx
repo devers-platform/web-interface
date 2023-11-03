707cccac8e30f0592a16390cfd0ae97925030320
Hoarfroster <hoarfroster@hoarfroster.space>
2023-11-03 22:25:59 +0800

    change: use header and footer in default layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
-import './globals.css'
+import './globals.scss'
 import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
+import Header from "@/app/header";
+import Footer from "@/app/footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
     title: '开发者技术社区',
@@ -10,15 +12,19 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-    <body className={inter.className}>
-    {children}
-    </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+        <body className={inter.className}>
+        <Header/>
+        <main className="container mx-auto px-8 py-16 lg:py-32">
+            {children}
+        </main>
+        <Footer />
+        </body>
+        </html>
+    )
 }
