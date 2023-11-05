cbb6d44bc983233bed012e952b59183f7e406fb4
hoarfroster <hoarfroster@outlook.com>
2023-11-05 20:53:17 +0800

    feat: add privacy policy and term of service pages

---
+++ app/about/privacy-policy/page.tsx
@@ -0,0 +1,8 @@
+export default function PrivacyPolicy() {
+    return (
+        <>
+            <h1>Privacy Policy</h1>
+            TODO: add Privacy Policy content.
+        </>
+    )
+}
---
+++ app/about/term-of-service/page.tsx
@@ -0,0 +1,8 @@
+export default function TermOfService() {
+    return (
+        <>
+            <h1>Term of Service</h1>
+            TODO: add Term of Service content.
+        </>
+    )
+}
