919ef26796df85418e13bb83594bf2b28ca1d6b8
hrein <739726896@qq.com>
2023-11-10 20:52:38 +0800

    create a template profile page

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,4 +1,6 @@
 import {User} from '@/app/user/usermodel'
+import Logo from "@/app/components/logo";
+import Link from "next/link";
 
 export default function UserProfile() {
     let user: User
@@ -7,8 +9,15 @@ export default function UserProfile() {
         user = User.testData()
     } else {
         // TODO: get user information from server
+        // let res = await fetch('http://localhost:3000/api/user')
+        // let data = await res.json()
+        // user = User.from(data)
         user = User.empty()
     }
 
-    return <></>
+    return <>
+        <h1 className="text-2xl font-bold leading-9 tracking-tight">{user.name}</h1>
+        <p className="text-sm text-gray-600 dark:text-gray-400">{user.id}</p>
+        <p className="text-sm text-gray-600 dark:text-gray-400">{user.createdAt.toLocaleString()}</p>
+        </>
 }
