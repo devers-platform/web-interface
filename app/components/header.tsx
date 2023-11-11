bbb629ebf1f0237beb527820fceadf0127976edc
hoarfroster <hoarfroster@outlook.com>
2023-11-11 21:58:27 +0800

    fix: incorrect bg color on header side-menu

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -95,7 +95,7 @@ export default function Header() {
                 onClose={setMobileMenuOpen}
                 aria-label="header-nav-sm:menu">
                 <div className="fixed inset-0 z-50" />
-                <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white dark:bg-slate-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
                         {/*
                             The brand of the dialog menu.
