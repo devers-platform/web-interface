22afff0c691b21e47e0ed6d4d43a325945975a18
hoarfroster <hoarfroster@outlook.com>
2023-11-11 21:44:56 +0800

    feat: update user profile layout

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,5 +1,8 @@
-import {Avatar, avatars, majors, User} from '@/app/user/usermodel'
-import Image from "next/image";
+import {User, userAvatars, userMajors} from '@/app/user/usermodel'
+import {faAdd} from '@fortawesome/free-solid-svg-icons/faAdd'
+import {faShare} from '@fortawesome/free-solid-svg-icons/faShare'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import Image from 'next/image'
 
 export default function UserProfile() {
     let user: User
@@ -13,35 +16,58 @@ export default function UserProfile() {
         // user = User.from(data)
         user = User.empty()
     }
-    let avatar = avatars.find((e) => e.avatar == user.avatar)!;
-
-    return <>
-        <div className="flex flex-col bg-white dark:bg-slate-900 px-4 py-1.5">
-            <div className="flex bg-gray-100 dark:bg-gray-800 p-4 rounded-md items-center">
-                <div className="w-1/2">
-                    <Image src={avatar.link} alt={avatar.name} width={200} height={200} className="mt-4"/>
+    let avatar = userAvatars.find((e) => e.avatar == user.avatar)!,
+        major = userMajors.find((e) => e.major == user.major)!
+    return (
+        <>
+            <div className="relative mx-auto my-8 flex max-w-6xl rounded bg-white px-8 py-4 dark:bg-slate-900">
+                <div className="-mt-8">
+                    <Image
+                        src={avatar.link}
+                        alt={avatar.name}
+                        width={160}
+                        height={160}
+                        className="rounded-xl border-4 border-white bg-slate-200 dark:border-slate-400 dark:bg-slate-800"
+                    />
                 </div>
-                <div className="w-1/2 pl-4">
-                    <h1 className="text-xl font-bold leading-9 tracking-tight">{user.name}</h1>
-                    <p className="text-sm text-gray-600 dark:text-gray-400">{user.id}</p>
-                    <p className="text-sm text-gray-600 dark:text-gray-400">{user.status}</p>
+                <div className="my-4 ml-8 grow">
+                    <h1 className="pb-4 text-3xl font-bold leading-9 tracking-tight">
+                        {user.name}
+                    </h1>
+                    <div className="space-y-2 text-gray-600 dark:text-gray-400">
+                        <p>
+                            <span className="inline-block w-[128px]">
+                                唯一识别符 ID
+                            </span>
+                            <span>{user.id}</span>
+                        </p>
+                        <p>
+                            <span className="inline-block w-[128px]">
+                                学校个人 ID
+                            </span>
+                            <span>{user.schoolId}</span>
+                        </p>
+                        <p>
+                            <span className="inline-block w-[128px]">专业</span>
+                            <span>{major.name}</span>
+                        </p>
+                        <p>
+                            <span className="inline-block w-[128px]">
+                                个人简介
+                            </span>
+                            <span>{user.bio}</span>
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div className="flex mt-4">
-                <div className="w-2/3 bg-gray-100 dark:bg-gray-800 p-4 rounded-md">
-                    <p className="text-sm text-gray-600 dark:text-gray-400">{user.schoolId}</p>
-                    <p className="text-sm text-gray-600 dark:text-gray-400">{majors.find((e) => e.major == user.major)?.name}</p>
-                    <p className="text-sm text-gray-600 dark:text-gray-400">{user.bio}</p>
-                    <button className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        收藏
+                <div className="my-4">
+                    <button className="mx-2 rounded bg-blue-600 px-4 py-2 dark:hover:bg-blue-700">
+                        <FontAwesomeIcon icon={faAdd} /> 关注
+                    </button>
+                    <button className="mx-2 rounded bg-blue-600 px-4 py-2 dark:hover:bg-blue-700">
+                        <FontAwesomeIcon icon={faShare} /> 分享
                     </button>
                 </div>
-                <div className="w-1/3 bg-gray-100 dark:bg-gray-800 p-4 rounded-md ml-4">
-                    <p className="text-sm text-gray-600 dark:text-gray-400">{user.points}</p>
-                    <p className="text-sm text-gray-600 dark:text-gray-400">{user.createdAt.toLocaleString()}</p>
-                    <p className="text-sm text-gray-600 dark:text-gray-400">{user.updatedAt?.toLocaleString()}</p>
-                </div>
             </div>
-        </div>
-    </>
+        </>
+    )
 }
